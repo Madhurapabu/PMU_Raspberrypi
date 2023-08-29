c1092afa32ec5fecdf3bd72d270d5287f0934567
Madhurapabu <madhurapabu@gmail.com>
2023-08-29 04:28:04 +0100

    style: :art: Change to One scritp

--- 3ph_phasor_estimation.py
+++ 3ph_phasor_estimation.py
@@ -12,13 +12,13 @@ numSamples = 20
 
 async def applyHammingWindow_ph1(data):
     for i in range(numSamples):
-        window = 1.5 - 0.2 * math.cos(2 * math.pi * i / (numSamples - 1))
+        window = 0.56 - 0.42 * math.cos(2 * math.pi * i / (numSamples - 1))
         data[i] *= window
 
 
 async def applyHammingWindow_ph2(data):
     for i in range(numSamples):
-        window = 1.5 - 0.2 * math.cos(2 * math.pi * i / (numSamples - 1))
+        window = 0.56 - 0.42 * math.cos(2 * math.pi * i / (numSamples - 1))
         data[i] *= window
 
 
@@ -60,12 +60,12 @@ if __name__ == '__main__':
                 numSamples = 20
                 prev_value = 0
 
-                lowPassAlpha = 0.05
+                lowPassAlpha = 0.1
                 lowPassFilteredMagnitude_ph1 = 0.00
                 lowPassFilteredMagnitude_ph2 = 0.0
                 lowPassFilteredMagnitude_ph3 = 0.0
 
-                highPassAlpha = 0.02
+                highPassAlpha = 0.1
                 highPassFilteredMagnitude_ph1 = 0.00
                 highPassFilteredMagnitude_ph2 = 0.0
                 highPassFilteredMagnitude_ph3 = 0.0
@@ -75,6 +75,8 @@ if __name__ == '__main__':
                 rawValue_ph3 = 0.0
 
                 frequency = 0.0
+                
+                frequency_2 = 0.0
 
                 f_time_3 = 0.0
                 frequency_3 = 0.0
@@ -82,16 +84,23 @@ if __name__ == '__main__':
                 count = 0
                 count_3 = 0
 
-                lowPassAlpha_fre = 0.008
+                lowPassAlpha_fre = 0.001
                 lowPassFrq = 0.0
                 lowPassFrq_3 = 0.0
 
                 highPassAlpha_freq = 0.01
                 highPassFrq = 0.0
                 highPassFrq_3 = 0.0
+                
+                prev_value_ph1= 0.0
+                prev_value_ph2= 0.0
+                prev_value_ph3= 0.0
 
-                time = 0.0
-
+                f_time = 0.0
+                f_time_3 = 0.0
+                
+                prv_lst = 0.0
+    
                 # Sending a message from the client
                 while True:
                     realPart_ph1 = [0.0] * numSamples
@@ -145,7 +154,7 @@ if __name__ == '__main__':
                             imagPart_ph3[i] = rawValue_ph3 * \
                                 math.sin(2 * math.pi * i / numSamples)
 
-                            if (i == 10):
+                            if (i == 12):
                                 try:
                                     # Connect to NTP server
                                     ntp_client = ntplib.NTPClient()
@@ -161,7 +170,7 @@ if __name__ == '__main__':
                                 except (ntplib.NTPException, ConnectionError) as e:
                                     print('Error retrieving NTP time:', e)
 
-                            if count_3 == 1:
+                            if count_3 == 2:
                                 f_time_3 = time.time() - f_time_3
                                 frequency_3 = 1/(f_time_3)
                                 f_time_3 = time.time()
@@ -174,40 +183,21 @@ if __name__ == '__main__':
                                     (lowPassFrq_3 - highPassFrq_3) + \
                                     highPassFrq_3
 
-                            combined_message_3 = f"f3,{highPassFrq_3}"
-                            await websocket.send(combined_message_3)
+                            # combined_message_3 = f"f3,{highPassFrq_3}"
+                            # await websocket.send(combined_message_3)
 
-                            if count == 2:
+                            if count == 10:
                                 f_time = time.time() - f_time
-                                frequency = 1/(f_time)
+                                frequency_2 = 1/(f_time)
                                 f_time = time.time()
                                 count = 0
-                                try:
-                                    # Connect to NTP server
-                                    ntp_client = ntplib.NTPClient()
-                                    response = ntp_client.request(
-                                        'pool.ntp.org', version=3, timeout=1)
-                                    ntp_time = datetime.datetime.fromtimestamp(
-                                        response.tx_time)
-                                    time_only = ntp_time.time()
-                                    adjusted_time = (datetime.datetime.combine(datetime.datetime.today(), time_only) +
-                                                     datetime.timedelta(hours=4, minutes=30)).time()
-                                    # print(adjusted_time)
 
-                                except (ntplib.NTPException, ConnectionError) as e:
-                                    print('Error retrieving NTP time:', e)
-
-                                lowPassFrq = lowPassAlpha_fre * frequency + \
+                                lowPassFrq = lowPassAlpha_fre * frequency_2 + \
                                     (1 - lowPassAlpha_fre) * lowPassFrq
 
                                 highPassFrq = highPassAlpha_freq * \
                                     (lowPassFrq - highPassFrq) + \
                                     highPassFrq
-
-                                print("fre:", highPassFrq)
-
-                                combined_message_2 = f"f2,{highPassFrq},{adjusted_time}"
-                                await websocket.send(combined_message_2)
 
                         except ValueError:
                             print("Error: Could not convert data to integer")
@@ -264,6 +254,7 @@ if __name__ == '__main__':
                     magnitude_ph3 = math.sqrt(
                         sumReal_ph3 ** 2 + sumImag_ph3 ** 2)
 
+
                     # Filter the amplitude value
                     lowPassFilteredMagnitude_ph1 = lowPassAlpha * magnitude_ph1 + \
                         (1 - lowPassAlpha) * lowPassFilteredMagnitude_ph1
@@ -283,17 +274,47 @@ if __name__ == '__main__':
                     highPassFilteredMagnitude_ph3 = highPassAlpha * \
                         (lowPassFilteredMagnitude_ph3 - highPassFilteredMagnitude_ph3) + \
                         highPassFilteredMagnitude_ph3
+                    
+                        
 
-                    scale_mag_ph1 = 1.21 * highPassFilteredMagnitude_ph1 - 0.86
-                    scale_mag_ph2 = 1.23 * highPassFilteredMagnitude_ph2 - 0.86
-                    scale_mag_ph3 = 1.25 * highPassFilteredMagnitude_ph3 - 0.86
+                    # if lowPassFilteredMagnitude_ph2 >= 20:
+                    #     lowPassAlpha = 0.002
+                    # if highPassFilteredMagnitude_ph2 >= 20:
+                    #     highPassFilteredMagnitude_ph1 = 0.002
+                    # if abs(prv_lst - highPassFilteredMagnitude_ph2) >= 1 :
+                    #     lowPassAlpha = 0.1
+                    #     highPassAlpha = 0.1
 
-                    print("Scaled Magnitude Ph1", scale_mag_ph1)
-                    print("Scaled Magnitude Ph2:", scale_mag_ph2)
-                    print("Scaled Magnitude Ph3:", scale_mag_ph3)
-                    print("Scaled Freq", frequency)
+                         
 
-                    combined_message = f"v,{scale_mag_ph1},{scale_mag_ph2},{scale_mag_ph3},{frequency},{adjusted_time}"
+                    scale_mag_ph1 = -3.7719 + (1.3029*highPassFilteredMagnitude_ph1)
+                    
+                    scale_mag_ph2 = -3.7719 + (1.2539*highPassFilteredMagnitude_ph2)
+                    #o = (-0.009920634920635088*highPassFilteredMagnitude_ph2**2) + (4.454365079365151*highPassFilteredMagnitude_ph2)  -258.0877976190549
+                    #y=-3.7719+1.2649x
+                    
+                    scale_mag_ph3 =  -3.7719 + (1.2139*highPassFilteredMagnitude_ph3)
+                    
+                    if scale_mag_ph2 >= 220 :
+                        
+                        lowPassAlpha = 0.01
+                        highPassAlpha = 0.1
+                        
+                    
+                    if scale_mag_ph3 >= 220 :
+                        
+                        lowPassAlpha = 0.01
+                        highPassAlpha = 0.1
+                        
+                    
+
+                    #print("Scaled Magnitude Ph1", scale_mag_ph1)
+                    #print("Scaled Magnitude Ph2:", scale_mag_ph2)
+                    #print("Scaled Magnitude Ph3:", scale_mag_ph3)
+                    #print("Scaled Freq", frequency)
+
+                    combined_message = f"v,{scale_mag_ph1},{scale_mag_ph2},{scale_mag_ph3},{frequency},{highPassFrq},{highPassFrq_3},{adjusted_time}"
                     # await websocket.send(str(scale_mag),str(frequency))  # Convert count to a string
+                    print(combined_message)
                     await websocket.send(combined_message)
     asyncio.get_event_loop().run_until_complete(connect())
